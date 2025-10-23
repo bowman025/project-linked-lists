f248d29fdf9d6fc332091e0d220312c697175437
RstK <208619198+bowman025@users.noreply.github.com>
2025-10-23 15:52:50 +0200

    Create files and basic setup

---
+++ linked-list.js
@@ -0,0 +1,48 @@
+export default class LinkedList {
+    constructor() {
+        
+    }
+    append(value) {
+
+    }
+    prepend(value) {
+
+    }
+    size() {
+
+    }
+    head() {
+
+    }
+    tail() {
+
+    }
+    at(index) {
+
+    }
+    pop() {
+
+    }
+    contains(value) {
+
+    }
+    find(value) {
+
+    }
+    toString() {
+
+    }
+    insertAt(value, index) {
+
+    }
+    removeAt(index) {
+
+    }
+}
+
+class Node {
+    constructor(value = null, nextNode = null) {
+        this.value = value;
+        this.nextNode = nextNode;
+    }
+}
